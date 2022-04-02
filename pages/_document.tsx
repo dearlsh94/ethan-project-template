2e8a84e065f8bb5f5c784a0b751e346cf668ebc2
dearlsh94 <dearlsh94@gmail.com>
2022-04-02 18:06:04 +0900

    init commit

---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class EthanDocument extends Document {
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          {/* NOTE for Material */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+EthanDocument.getInitialProps = async (ctx) => {
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps
+  }
+}
+
+export default EthanDocument
