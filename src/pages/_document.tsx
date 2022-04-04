e05ff6f295e9fe991ae18311434b92bdc76e3583
dearlsh94 <dearlsh94@gmail.com>
2022-04-04 22:40:37 +0900

    * prettier, eslint 실행

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,35 +2,29 @@ import * as React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class EthanDocument extends Document {
-  render() {
-    return (
-      <Html lang="ko">
-        <Head>
-          {/* NOTE for Material */}
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html lang="ko">
+				<Head>
+					{/* NOTE for Material */}
+					<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+					<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
 
 EthanDocument.getInitialProps = async (ctx) => {
-  const initialProps = await Document.getInitialProps(ctx)
+	const initialProps = await Document.getInitialProps(ctx)
 
-  return {
-    ...initialProps
-  }
+	return {
+		...initialProps,
+	}
 }
 
 export default EthanDocument
--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -2,64 +2,66 @@ import NextErrorComponent, { ErrorProps as NextErrorProps } from 'next/error'
 import { NextPageContext } from 'next'
 
 export type ErrorPageProps = {
-  err: Error
-  statusCode: number
-  isReadyToRender: boolean
-  children?: React.ReactElement
+	err: Error
+	statusCode: number
+	isReadyToRender: boolean
+	children?: React.ReactElement
 }
 
 export type ErrorProps = {
-  isReadyToRender: boolean
+	isReadyToRender: boolean
 } & NextErrorProps
 
 const ErrorPage = (props: ErrorPageProps): JSX.Element => {
-  const { statusCode, isReadyToRender, err, children = null } = props
+	const { statusCode, isReadyToRender, err, children = null } = props
 
-  console.log('statusCode : ', statusCode)
-  
-  if (process.env.NEXT_PUBLIC_APP_STAGE !== 'development') {
-    console.warn('ErrorPage - details:')
-    console.error(err)
-  }
+	console.log('statusCode : ', statusCode)
 
-  if (!isReadyToRender && err) {
-  }
+	if (process.env.NEXT_PUBLIC_APP_STAGE !== 'development') {
+		console.warn('ErrorPage - details:')
+		console.error(err)
+	}
 
-  return <>{children ?? <NextErrorComponent statusCode={statusCode} />}</>
+	if (!isReadyToRender && err) {
+		console.error(err)
+	}
+
+	return <div>{children ?? <NextErrorComponent statusCode={statusCode} />}</div>
 }
 
 export async function getServerSideProps(props: NextPageContext) {
-  const { res, err, asPath } = props
+	const { res, err, asPath } = props
+	console.log(res, asPath)
 
-  const errorInitialProps: ErrorProps =
-    (await NextErrorComponent.getInitialProps({
-      res,
-      err,
-    } as NextPageContext)) as ErrorProps
+	const errorInitialProps: ErrorProps = (await NextErrorComponent.getInitialProps({
+		res,
+		err,
+	} as NextPageContext)) as ErrorProps
 
-  if (process.env.NEXT_PUBLIC_APP_STAGE !== 'production') {
-    console.error('ErrorPage.getInitialProps - details :', err)
-  }
+	if (process.env.NEXT_PUBLIC_APP_STAGE !== 'production') {
+		console.error('ErrorPage.getInitialProps - details :', err)
+	}
 
-  errorInitialProps.isReadyToRender = true
+	errorInitialProps.isReadyToRender = true
 
-  if (res?.statusCode === 404) {
-    return {
-      props: {
-        statusCode: 404, isReadyToRender: true
-      }
-    }
-  }
+	if (res?.statusCode === 404) {
+		return {
+			props: {
+				statusCode: 404,
+				isReadyToRender: true,
+			},
+		}
+	}
 
-  if (err) {
-    return {
-      props: { ...errorInitialProps }
-    }
-  }
+	if (err) {
+		return {
+			props: { ...errorInitialProps },
+		}
+	}
 
-  return {
-    props: { ...errorInitialProps }
-  }
+	return {
+		props: { ...errorInitialProps },
+	}
 }
 
 export default ErrorPage
